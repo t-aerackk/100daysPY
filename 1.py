6e9eda81139b34bb700173e02cfe03d4bd2d57f7
t-aerackk <Aenjaal@gmail.com>
2024-02-12 20:05:29 +0545

    update

--- 1.py
+++ 1.py
@@ -1,2 +1,15 @@
 #My journey towards 100 days python code starts at feb 10 2024 i'll try to be as consistent as possible.
-print("Hello 2024")
+print("Hello 2024") #Single line
+print('''hello 
+      my name
+      is Anjal''') 
+
+name="Anjal"
+age=26
+print(f"Hello my name is {name} and i'm {age} years old.")
+print("Hello my name is {} and I'm {} years old.".format(name, age))
+print("Hello my name is %s and I'm %d years old."% (name, age))
+
+
+
+
